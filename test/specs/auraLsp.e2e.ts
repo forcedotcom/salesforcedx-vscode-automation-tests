8a9e0fb550be57b438d99c96cf7243d87be43c9c
CristiCanizales <cristina.canizales@salesforce.com>
2023-05-05 15:37:47 -0300

    feat: go to definition, verify is running and autocompletion

---
+++ test/specs/auraLsp.e2e.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { step } from 'mocha-steps';
+import { TextEditor } from 'wdio-vscode-service';
+import { TestSetup } from '../testSetup';
+import * as utilities from '../utilities';
+
+describe('Aura LSP', async () => {
+  let testSetup: TestSetup;
+  let projectName: string;
+
+  step('Set up the testing environment', async () => {
+    testSetup = new TestSetup('AuraLsp', true);
+    await testSetup.setUp();
+    projectName = testSetup.tempProjectName.toUpperCase();
+
+    // Create Aura Component
+    await utilities.createAura('aura1');
+  });
+
+  step('Verify Extension is Running', async () => {
+    // Using the Command palette, run Developer: Show Running Extensions
+    const workbench = await browser.getWorkbench();
+    await utilities.runCommandFromCommandPrompt(
+      workbench,
+      'Developer: Show Running Extensions',
+      1
+    );
+
+    // Verify Aura Components extension is present and running
+    const extensionNameDivs = await $$('div.name');
+    let extensionWasFound = false;
+    for (const extensionNameDiv of extensionNameDivs) {
+      const text = await extensionNameDiv.getText();
+      if (text.includes('salesforce.salesforcedx-vscode-lightning')) {
+        extensionWasFound = true;
+      }
+    }
+    expect(extensionWasFound).toBe(true);
+  });
+
+  step('Go to Definition', async () => {
+    // Get open text editor
+    const workbench = await browser.getWorkbench();
+    const editorView = workbench.getEditorView();
+    const textEditor = (await editorView.openEditor('aura1.cmp')) as TextEditor;
+    await textEditor.moveCursor(3, 57);
+
+    // Go to definition through F12
+    await browser.keys(['F12']);
+    await utilities.pause(1);
+
+    //Verify 'Go to definition' took us to the definition file
+    const activeTab = await editorView.getActiveTab();
+    const title = await activeTab?.getTitle();
+    expect(title).toBe('aura1.js');
+  });
+
+  step('Autocompletion', async () => {
+    // Get open text editor
+    const workbench = await browser.getWorkbench();
+    const editorView = workbench.getEditorView();
+    const textEditor = (await editorView.openEditor('aura1.cmp')) as TextEditor;
+    await textEditor.typeTextAt(3, 11, ' if');
+    await utilities.pause(1);
+
+    // Verify autocompletion options are present
+    const autocompletionOptions = await $$(
+      'textarea.inputarea.monaco-mouse-cursor-text'
+    );
+    expect(await autocompletionOptions[0].getAttribute('aria-haspopup')).toBe(
+      'true'
+    );
+    expect(
+      await autocompletionOptions[0].getAttribute('aria-autocomplete')
+    ).toBe('list');
+  });
+
+  step('Tear down and clean up the testing environment', async () => {
+    await testSetup.tearDown();
+  });
+});
