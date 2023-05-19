047106ee955414f6f3f4be598dee0d693284439b
Cristina Cañizales <113132642+CristiCanizales@users.noreply.github.com>
2023-05-19 15:37:10 -0300

    Cristi/lwc lsp (#10)
    * feat: go to definition (js) and other tests in progress
    
    * chore: more structure for the tests
    
    * chore: fix for extension not loading + html goto definition
    
    * chore: autocompletion - done
    
    * chore: revert config changes
    
    * chore: removed space
    
    * chore: added spec to run test individually
    
    * chore: added logging everywhere
    
    * chore: indent and verify autocompletion works
    
    * chore: added comment
    
    * chore: right indentation
    
    * chore: space in comment

---
+++ test/utilities/lwcUtils.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { TextEditor } from 'wdio-vscode-service';
+import { runCommandFromCommandPrompt } from './commandPrompt';
+import { log, pause } from './miscellaneous';
+
+export async function enableLWCExtension(): Promise<void> {
+  // salesforce.salesforcedx-vscode-lwc extension is actually not loading automatically
+  // because it depends on ESLint and it's not installed by default, so we need to
+  // Install it, reload and enable extensions to get them both running
+  log('');
+  log('enableLWCExtension() - calling browser.getWorkbench()');
+  const workbench = await browser.getWorkbench();
+
+  log('enableLWCExtension() - getting buttons with selector');
+  let buttons = await $$('a.monaco-button.monaco-text-button');
+  for (const item of buttons) {
+    const text = await item.getText();
+    if (text.includes('Install and Reload')) {
+      log('enableLWCExtension() - Install and Reload');
+      await item.click();
+    }
+  }
+  await pause(10);
+
+  log('enableLWCExtension() - getting buttons after reloading with selector');
+  buttons = await $$('a.monaco-button.monaco-text-button');
+  for (const item of buttons) {
+    const text = await item.getText();
+    if (text.includes('Reload and Enable Extensions')) {
+      log('enableLWCExtension() - Reload and Enable Extensions');
+      await item.click();
+    }
+  }
+  await pause(5);
+
+  log('enableLWCExtension() - Running Developer: Reload Window');
+  log('');
+  await runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 10);
+}
+
+export async function createLWC(name: string): Promise<void> {
+  log('createLWC() - calling browser.getWorkbench()');
+  const workbench = await browser.getWorkbench();
+
+  log(`createLWC() - Running SFDX: Create Lightning Web Component`);
+  // Using the Command palette, run SFDX: Create Lightning Web Component.
+  let inputBox = await runCommandFromCommandPrompt(
+    workbench,
+    'SFDX: Create Lightning Web Component',
+    1
+  );
+
+  log(`createLWC() - Set the name of the new component`);
+  // Set the name of the new component
+  await inputBox.setText(name);
+  await inputBox.confirm();
+  await pause(1);
+
+  log(`createLWC() - Select the default directory`);
+  // Select the default directory (press Enter/Return).
+  await inputBox.confirm();
+  await pause(3);
+
+  log(`createLWC() - Modify js content`);
+  // Modify js content
+  const editorView = workbench.getEditorView();
+  let textEditor = (await editorView.openEditor(name + '.js')) as TextEditor;
+  const jsText = [
+    `import { LightningElement } from 'lwc';`,
+    ``,
+    `export default class ${name} extends LightningElement {`,
+    `\tgreeting = 'World';`,
+    `}`
+  ].join('\n');
+  await textEditor.setText(jsText);
+  await textEditor.save();
+  await pause(1);
+
+  log(`createLWC() - Modify html content`);
+  log('');
+  // Modify html content
+  inputBox = await runCommandFromCommandPrompt(workbench, 'Go to File...', 1);
+  await inputBox.setText(name + '.html');
+  await inputBox.confirm();
+  await pause(1);
+
+  const htmlText = [
+    `<template>`,
+    `\t<lightning-card title="${name}" icon-name="custom:custom14">`,
+    `\t\t<div class="slds-var-m-around_medium">Hello, {greeting}!</div>`,
+    `\t\t<c-view-source source="lwc/hello" slot="footer">`,
+    `\t\t\tBind an HTML element to a component property.`,
+    `\t\t</c-view-source>`,
+    `\t</lightning-card>`,
+    `</template>`
+  ].join('\n');
+  await textEditor.setText(htmlText);
+  await textEditor.save();
+  await pause(1);
+}
