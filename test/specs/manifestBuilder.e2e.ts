c113c8cd74c4b298a4c2a830db480fed19c3d2fc
CristiCanizales <cristina.canizales@salesforce.com>
2023-05-24 13:01:30 -0300

    chore: sorted imports

--- test/specs/manifestBuilder.e2e.ts
+++ test/specs/manifestBuilder.e2e.ts
@@ -4,12 +4,11 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import path from 'path';
 import { step } from 'mocha-steps';
 import { TestSetup } from '../testSetup';
-import * as utilities from '../utilities';
-import * as fs from 'fs-extra';
-import path from 'path';
 import { TextEditor } from 'wdio-vscode-service';
+import * as utilities from '../utilities';
 
 describe('Manifest Builder', async () => {
   let testSetup: TestSetup;
