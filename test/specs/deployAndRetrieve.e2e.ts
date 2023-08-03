2514b73d2abab2961e8108c857370448d4482b24
CristiCanizales <cristina.canizales@salesforce.com>
2023-08-03 17:00:05 -0300

    chore: increase pause after disabling st

--- test/specs/deployAndRetrieve.e2e.ts
+++ test/specs/deployAndRetrieve.e2e.ts
@@ -422,7 +422,7 @@ describe('Deploy and Retrieve', async () => {
     await enableSourceTrackingBtn.click();
     await utilities.pause(1);
     // Reload window to update cache and get the setting behavior to work
-    await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 30);
+    await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 50);
   });
 
   step('Deploy with SFDX: Deploy This Source to Org - ST disabled', async () => {
