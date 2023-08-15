3853fabde975889fb23cf7b6a904c06495d2d3d4
CristiCanizales <cristina.canizales@salesforce.com>
2023-08-15 14:02:16 -0300

    chore: increase pause + remove unused import

--- test/specs/lwcLsp.e2e.ts
+++ test/specs/lwcLsp.e2e.ts
@@ -30,7 +30,7 @@ describe('LWC LSP', async () => {
     await utilities.showRunningExtensions(workbench);
     await utilities.enableLwcExtension();
     // Zoom out so more extensions are visible
-    await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 20);
+    await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 60);
     await utilities.runCommandFromCommandPrompt(workbench, 'View: Zoom Out', 2);
 
     // Verify Lightning Web Components extension is present and running
