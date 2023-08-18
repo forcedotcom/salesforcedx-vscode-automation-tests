452e68bfb9650fbb62c45913e3d9593f68aca513
CristiCanizales <cristina.canizales@salesforce.com>
2023-08-18 15:24:39 -0300

    chore: double reload and notifWithTimeout

--- test/specs/trailApexReplayDebugger.e2e.ts
+++ test/specs/trailApexReplayDebugger.e2e.ts
@@ -115,6 +115,7 @@ describe('"Find and Fix Bugs with Apex Replay Debugger" Trailhead Module', async
     // Calling SFDX: Update Checkpoints in Org fails if the org has been recently created,
     // it does not complete the 6 steps but only 4.
     // Reloading the window forces the extensions to be reloaded and this seems to fix the issue.
+    await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 10);
     await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 30);
 
     // Verify checkpoint is present
@@ -150,18 +151,11 @@ describe('"Find and Fix Bugs with Apex Replay Debugger" Trailhead Module', async
       10
     );
 
-    // Wait for the command to execute
-    await utilities.waitForNotificationToGoAway(
-      workbench,
-      'Running SFDX: Turn On Apex Debug Log for Replay Debugger',
-      utilities.FIVE_MINUTES
-    );
-    await utilities.pause(10);
-
     // Look for the success notification that appears which says, "SFDX: Turn On Apex Debug Log for Replay Debugger successfully ran".
-    const successNotificationWasFound = await utilities.notificationIsPresent(
+    const successNotificationWasFound = await utilities.notificationIsPresentWithTimeout(
       workbench,
-      'SFDX: Turn On Apex Debug Log for Replay Debugger successfully ran'
+      'SFDX: Turn On Apex Debug Log for Replay Debugger successfully ran',
+      utilities.FIVE_MINUTES
     );
     expect(successNotificationWasFound).toBe(true);
 
