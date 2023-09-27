7692af4d7cbb4721b332cba5d3e4e976329a132b
Daphne Yang <139700604+daphne-sfdc@users.noreply.github.com>
2023-09-27 18:26:40 -0400

    Visualforce E2E Test - zoom out to make Visualforce extension visible in running extensions (#50)
    * zoom out to see all running extensions
    
    * decrease pause of zoom out from 5 seconds to 2 seconds

--- test/specs/visualforceLsp.e2e.ts
+++ test/specs/visualforceLsp.e2e.ts
@@ -65,6 +65,10 @@ describe('Visualforce LSP', async () => {
     // Using the Command palette, run Developer: Show Running Extensions
     const workbench = await (await browser.getWorkbench()).wait();
     await utilities.showRunningExtensions(workbench);
+    await utilities.runCommandFromCommandPrompt(workbench, 'View: Zoom Out', 2);
+    await utilities.runCommandFromCommandPrompt(workbench, 'View: Zoom Out', 2);
+    await utilities.runCommandFromCommandPrompt(workbench, 'View: Zoom Out', 2);
+    await utilities.runCommandFromCommandPrompt(workbench, 'View: Zoom Out', 2);
 
     // Verify Visualforce extension is present and running
     const extensionWasFound = await utilities.findExtensionInRunningExtensionsList(
