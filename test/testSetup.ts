9c72d8304909e6db0502868dd5651e94950f8c9c
CristiCanizales <cristina.canizales@salesforce.com>
2023-10-18 20:43:33 +0200

    chore: chore

--- test/testSetup.ts
+++ test/testSetup.ts
@@ -51,7 +51,7 @@ export class TestSetup {
     // throw Error();
     await this.setUpTestingEnvironment();
     await this.createProject(scratchOrgEdition);
-    // await utilities.reloadAndEnableExtensions();
+    await utilities.reloadAndEnableExtensions();
     await this.authorizeDevHub();
     await this.createDefaultScratchOrg();
     await this.disableCommandCenter();
