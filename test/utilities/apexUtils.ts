6111540e4c603a62ec5c6b220ae500735b0a3f57
Daphne Yang <daphne.yang@salesforce.com>
2023-10-18 15:36:38 -0400

    try replacing inputBox.confirm() with browser.keys(['Enter']) again in createApexClass() in apexUtils.ts

--- test/utilities/apexUtils.ts
+++ test/utilities/apexUtils.ts
@@ -21,11 +21,12 @@ export async function createApexClass(
 
   // Set the name of the new Apex Class
   await inputBox.setText(name);
-  await inputBox.confirm();
+  await pause(1);
+  await browser.keys(['Enter']);
   await pause(1);
 
   // Select the default directory (press Enter/Return).
-  await inputBox.confirm();
+  await browser.keys(['Enter']);
   await pause(1);
 
   // Modify class content
