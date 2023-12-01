ab779a299d257c5be05b89a0d1aca5dd392c700b
CristiCanizales <cristina.canizales@salesforce.com>
2023-12-01 16:37:46 -0300

    chore: increase pause after installing extensions

--- test/utilities/extensionUtils.ts
+++ test/utilities/extensionUtils.ts
@@ -99,7 +99,7 @@ export async function installExtensions(): Promise<void> {
   for (const extension of extensions) {
     await installExtension(extension);
   }
-  await pause(150);
+  await pause(200);
   await runCommandFromCommandPrompt(workbench, 'Extensions: Enable All Extensions', 5);
   await runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 2);
 }
