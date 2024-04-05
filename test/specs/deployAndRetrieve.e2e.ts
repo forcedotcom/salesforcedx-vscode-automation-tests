7ed64ed9ad714ec461a512dc19264b7c01aa628e
CristiCanizales <cristina.canizales@salesforce.com>
2024-04-05 12:13:13 -0300

    chore: expect after click

--- test/specs/deployAndRetrieve.e2e.ts
+++ test/specs/deployAndRetrieve.e2e.ts
@@ -472,33 +472,34 @@ describe('Deploy and Retrieve', async () => {
       2
     );
 
-    // // Make sure we get a confirmation dialog
-    // const confirmationDialogText =
-    //   'Deleting source files deletes the files from your computer and removes the corresponding metadata from your default org. Are you sure you want to delete this source from your project and your org?, source: Salesforce CLI Integration, notification, Inspect the response in the accessible view with Option+F2';
-    // const confirmationDialogEl = await utilities.findElementByText(
-    //   'div',
-    //   'aria-label',
-    //   confirmationDialogText
-    // );
-    // expect(confirmationDialogEl).toBeTruthy();
+    // Make sure we get a confirmation dialog
+    const confirmationDialogText =
+      'Deleting source files deletes the files from your computer and removes the corresponding metadata from your default org. Are you sure you want to delete this source from your project and your org?, source: Salesforce CLI Integration, notification, Inspect the response in the accessible view with Option+F2';
+    const confirmationDialogEl = await utilities.findElementByText(
+      'div',
+      'aria-label',
+      confirmationDialogText
+    );
+    expect(confirmationDialogEl).toBeTruthy();
 
     // Confirm deletion
-    // const deleteSourceBtn = await utilities.findElementByText(
-    //   'a',
-    //   'class',
-    //   'monaco-button monaco-text-button'
-    // );
-    // await deleteSourceBtn.click();
-    const buttons = await $$('a.monaco-button.monaco-text-button');
-    for (const button of buttons) {
-      const text = await button.getText();
-      if (text.includes('Delete Source')) {
-        utilities.log('SFDX: Delete This from Project and Org - Delete Source button found');
-        await button.click();
-        break;
-      }
-    }
-    await utilities.pause(3);
+    const deleteSourceBtn = await utilities.findElementByText(
+      'a',
+      'class',
+      'monaco-button monaco-text-button'
+    );
+    await deleteSourceBtn.click();
+    // const buttons = await $$('a.monaco-button.monaco-text-button');
+    // for (const button of buttons) {
+    //   const text = await button.getText();
+    //   if (text.includes('Delete Source')) {
+    //     utilities.log('SFDX: Delete This from Project and Org - Delete Source button found');
+    //     await button.click();
+    //     break;
+    //   }
+    // }
+    await utilities.pause(10);
+    expect(1).toBe(2);
 
     const successNotificationWasFound = await utilities.notificationIsPresentWithTimeout(
       workbench,
