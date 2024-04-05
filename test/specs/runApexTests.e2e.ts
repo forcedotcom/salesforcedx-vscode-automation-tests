c76f576ee310156ca60d405a452bc8c00abcd31f
CristiCanizales <cristina.canizales@salesforce.com>
2024-04-05 13:01:51 -0300

    chore: run all tests on a class

--- test/specs/runApexTests.e2e.ts
+++ test/specs/runApexTests.e2e.ts
@@ -305,10 +305,18 @@ describe('Run Apex Tests', async () => {
     await utilities.runCommandFromCommandPrompt(workbench, 'View: Clear Output', 2);
 
     // Click the run test button that is shown to the right when you hover a test class name on the Test sidebar
-    const apexTestItem = (await apexTestsSection.findItem('ExampleApexClass2Test')) as TreeItem;
-    await apexTestItem.select();
-    const runTestsAction = await apexTestItem.getActionButton('Run Tests');
-    await runTestsAction!.elem.click();
+    // const apexTestItem = (await apexTestsSection.findItem('ExampleApexClass2Test')) as TreeItem;
+    // await apexTestItem.select();
+    // const runTestsAction = await apexTestItem.getActionButton('Run Tests');
+    // await runTestsAction!.elem.click();
+    const apexClassEl = await utilities.findElementByText(
+      'div',
+      'aria-label',
+      'ExampleApexClass2Test'
+    );
+    await apexClassEl.click();
+    const runTestsBtn = await apexClassEl.$('a[aria-label="Run Tests"]');
+    await runTestsBtn.click();
 
     // Look for the success notification that appears which says, "SFDX: Run Apex Tests successfully ran".
     const successNotificationWasFound = await utilities.notificationIsPresentWithTimeout(
