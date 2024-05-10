de27f01b50ea1a605062638b89f693d324f7a309
CristiCanizales <cristina.canizales@salesforce.com>
2024-05-10 02:33:46 +0200

    chore: skip ootb apex snippets and add back custom snippets

--- test/specs/miscellaneous.e2e.ts
+++ test/specs/miscellaneous.e2e.ts
@@ -4,10 +4,9 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { step } from 'mocha-steps';
+import { step, xstep } from 'mocha-steps';
 import { TestSetup } from '../testSetup';
 import * as utilities from '../utilities';
-import { CMD_KEY } from 'wdio-vscode-service/dist/constants';
 
 describe('Miscellaneous', async () => {
   let testSetup: TestSetup;
@@ -17,7 +16,7 @@ describe('Miscellaneous', async () => {
     await testSetup.setUp();
   });
 
-  step('Use out-of-the-box Apex Snippets', async () => {
+  xstep('Use out-of-the-box Apex Snippets', async () => {
     utilities.log(`${testSetup.testSuiteSuffixName} - Use Apex Snippets`);
     const workbench = await (await browser.getWorkbench()).wait();
     const apexSnippet = 'String.isBlank(inputString)';
@@ -28,18 +27,52 @@ describe('Miscellaneous', async () => {
     // Type snippet "isb" in a new line and check it inserted the expected string
     const textEditor = await utilities.getTextEditor(workbench, 'Anonymous.apex');
     await utilities.runCommandFromCommandPrompt(workbench, 'Snippets: Insert Snippet', 1);
-    // await browser.keys([CMD_KEY, 'f']);
-    // await utilities.pause(1);
-    // await browser.keys([`System.debug('¡Hola mundo!');`]);
-    // await browser.keys(['Escape']);
-    // await browser.keys(['ArrowRight', 'Enter']);
-    // await browser.keys(['isb']);
+    await browser.keys(['isb']);
     await utilities.pause(2);
-    expect(1).toBe(2);
     await browser.keys(['Enter']);
     await textEditor.save();
     const fileContent = await textEditor.getText();
     expect(fileContent).toContain(apexSnippet);
+  });
+
+  step('Use Custom Apex Snippets', async () => {
+    utilities.log(`${testSetup.testSuiteSuffixName} - Use Apex Snippets`);
+
+    // Using the Command palette, run Snippets: Configure User Snippets
+    const workbench = await (await browser.getWorkbench()).wait();
+    await utilities.runCommandFromCommandPrompt(workbench, 'Snippets: Configure User Snippets', 1);
+    await browser.keys(['New Global Snippets file...', 'Enter']);
+    await utilities.pause(1);
+    await browser.keys(['apex.json', 'Enter']);
+
+    const apexSnippet = [
+      `{`,
+      `"SOQL": {`,
+      `"prefix": "soql",`,
+      `"body": [`,
+      `  "[SELECT \${1:field1, field2} FROM \${2:SobjectName} WHERE \${3:clause}];"`,
+      `],`,
+      `"description": "Apex SOQL query"`,
+      `}`,
+      `}`
+    ].join('\n');
+
+    // Modify file content
+    const textEditor = await utilities.getTextEditor(workbench, 'apex.json.code-snippets');
+    await textEditor.setText(apexSnippet);
+    await textEditor.save();
+    await utilities.runCommandFromCommandPrompt(workbench, 'Developer: Reload Window', 50);
+
+    // Create anonymous apex file
+    await utilities.createAnonymousApexFile();
+    await browser.keys(['Enter']);
+
+    // Type snippet "soql" and check it inserted the expected query
+    await browser.keys(['soql']);
+    await utilities.pause(2);
+    await browser.keys(['Enter']);
+    const fileContent = await textEditor.getText();
+    expect(fileContent).toContain('[SELECT field1, field2 FROM SobjectName WHERE clause];');
   });
 
   step('Use out-of-the-box LWC Snippets - HTML', async () => {
