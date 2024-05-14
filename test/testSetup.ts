acc9c67585cdb2ae9d51059fedc0a6a9c6ee4b2e
CristiCanizales <cristina.canizales@salesforce.com>
2024-05-14 02:48:18 +0200

    chore: check window command center from wdioconfig

--- test/testSetup.ts
+++ test/testSetup.ts
@@ -63,7 +63,7 @@ export class TestSetup {
   public async disableCommandCenter(): Promise<void> {
     const workbench = await (await browser.getWorkbench()).wait();
     await utilities.runCommandFromCommandPrompt(workbench, 'Preferences: Open User Settings', 3);
-    await browser.keys(['autoupdate']);
+    await browser.keys(['Window: Command Center']);
     await utilities.pause(3);
     expect(1).toBe(2);
 
