c402663500933a1ae7784a4d135b6358dd5b0197
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2024-07-05 22:04:44 +0200

    chore: use executequickpick instead of runcommandfromcommandprompt

--- test/utilities/apexUtils.ts
+++ test/utilities/apexUtils.ts
@@ -108,7 +108,7 @@ export async function createAnonymousApexFile(): Promise<void> {
   const editorView = workbench.getEditorView();
 
   // Using the Command palette, run File: New File...
-  const inputBox = await runCommandFromCommandPrompt(workbench, 'Create: New File...', 1);
+  const inputBox = await executeQuickPick('Create: New File...', 1);
 
   // Set the name of the new Anonymous Apex file
   await inputBox.setText('Anonymous.apex');
