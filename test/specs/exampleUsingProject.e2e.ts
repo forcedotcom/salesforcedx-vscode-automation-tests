74f5d6c0eb717c5e6c7572d04db99bbbbc7a2870
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-08-20 21:36:52 -0700

    chore: refactor

--- test/specs/exampleUsingProject.e2e.ts
+++ test/specs/exampleUsingProject.e2e.ts
@@ -1,13 +1,14 @@
 import { step } from 'mocha-steps';
 import { TestSetup } from '../testSetup.ts';
 import * as utilities from '../utilities/index.ts';
+import * as constants from './constants.ts';
 
 describe('an example using existing project', async () => {
   let testSetup: TestSetup;
 
   step('Set up the testing environment', async () => {
     testSetup = new TestSetup('usingExistingProject');
-    await testSetup.setUp('developer', utilities.repoKeywords.DeployInv);
+    await testSetup.setUp('developer', constants.repoKeywords.DeployInv);
     await utilities.reloadAndEnableExtensions();
     await expect(1).toBe(false); // make it false to see screenshots
   });
