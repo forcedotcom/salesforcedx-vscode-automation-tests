ae98f8a28b259bd0e62dfe6d30200aaac9aa029c
Cristina Isabel Cañizales <cristina.canizales@salesforce.com>
2024-09-25 15:35:13 -0300

    feat: test for soql toggle button

--- test/environmentSettings.ts
+++ test/environmentSettings.ts
@@ -33,7 +33,8 @@ export class EnvironmentSettings {
     // './specs/**/orgBrowser.e2e.ts',
     // './specs/**/pushAndPull.e2e.ts',
     // './specs/**/runApexTests.e2e.ts',
-    // './specs/**/runLwcTests.e2e.ts'
+    // './specs/**/runLwcTests.e2e.ts',
+    // './specs/**/soql.e2e.ts',
     // './specs/**/sObjectsDefinitions.e2e.ts',
     // './specs/**/templates.e2e.ts',
     // './specs/**/trailApexReplayDebugger.e2e.ts',
